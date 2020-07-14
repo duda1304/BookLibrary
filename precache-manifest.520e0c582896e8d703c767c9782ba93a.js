a378501b9f143a7735bd113beeff78324b55905c
Duda13041981 <dubravkaspoljaric@gmail.com>
2020-07-14 18:19:23 +0200

    Updates

---
+++ precache-manifest.520e0c582896e8d703c767c9782ba93a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8949225e8c729415c10ee7df9e273560",
+    "url": "/BookLibrary/index.html"
+  },
+  {
+    "revision": "cb7f0fb10e2ca0525c5d",
+    "url": "/BookLibrary/static/css/main.31fa8438.chunk.css"
+  },
+  {
+    "revision": "53d0fcc15946be71996d",
+    "url": "/BookLibrary/static/js/2.8fea069d.chunk.js"
+  },
+  {
+    "revision": "db185181e97e095211720d876bff5692",
+    "url": "/BookLibrary/static/js/2.8fea069d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cb7f0fb10e2ca0525c5d",
+    "url": "/BookLibrary/static/js/main.a92df6dd.chunk.js"
+  },
+  {
+    "revision": "6cfa841693b36aa618d4",
+    "url": "/BookLibrary/static/js/runtime-main.5671d089.js"
+  }
+]);
